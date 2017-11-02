791c5bbfaed0390a61bf1dc7fb2248c34c7abced
Eric Neidhardt <eric.neidhardt@sevenval.com>
2017-11-02 11:27:46 +0100

    -cleanup

--- viewpager-dialog/src/main/kotlin/org/neidhardt/viewpagerdialog/viewhelper/ViewPagerDialogBuilder.kt
+++ viewpager-dialog/src/main/kotlin/org/neidhardt/viewpagerdialog/viewhelper/ViewPagerDialogBuilder.kt
@@ -8,7 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import org.neihdardt.viewpagerdialog.R
+import org.neidhardt.viewpagerdialog.R
 
 @Suppress("unused")
 /**
